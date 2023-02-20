2156b65659fec1b66a18d19caab5eedac67db793
eric artursson <eric.o.artursson@hotmail.com>
2023-02-20 22:29:08 +0100

    maybe works?

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@ pub fn mvc(pins: &mut Pins, mut x: u8, y: u8)
 {
     pins.rs.set_low();
     if y == 1 {
-        x+=44;
+        x+=47;
     }
     let x = decimals_to_binary(&vec![x]).unwrap();
     let mut x: String = x.iter().map(ToString::to_string).collect();
