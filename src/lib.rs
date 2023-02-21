72c09212c7cb7d529dfa06d908f60b1aa990fabb
eric artursson <eric.o.artursson@hotmail.com>
2023-02-21 14:20:38 +0100

    fixed stupid bug

--- src/lib.rs
+++ src/lib.rs
@@ -154,7 +154,7 @@ pub fn write(pins: &mut Pins, text: &str)
     let binary_text = string_to_binary(text).unwrap();
 
     for bits in binary_text{
-        bbwrite(pins, 8 as u8);
+        bbwrite(pins, bits as u8);
     }
 }
 
