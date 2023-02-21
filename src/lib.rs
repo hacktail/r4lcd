d775eda1e938c1c2bc44b617212075b9774612cc
eric artursson <eric.o.artursson@hotmail.com>
2023-02-21 16:47:19 +0100

    hjhjjhkmn

--- src/lib.rs
+++ src/lib.rs
@@ -186,7 +186,7 @@ pub fn bbwrite(pins: &mut Pins, bits: u8)
 
     
         println!("{:b}", bits & PIN_FLAGS[7]);
-        println!("{}",10000000 | bits);
+        println!("{}",0b10000000 | bits);
 
     sleep(Duration::from_millis(30));
     pulse(pins);
