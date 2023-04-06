612a7f505219d9584a1ca293038beba2039c6562
eric artursson <eric.o.artursson@hotmail.com>
2023-04-06 21:58:17 +0200

    removed old commented out code

--- src/pins/mod.rs
+++ src/pins/mod.rs
@@ -43,18 +43,3 @@ impl Pins {
         }
     }
 }
-
-
-
-//                Self {
-//            d0: gpio.get(26).expect("couldn't get pin").into_output_low(),
-//            d1: gpio.get(19).expect("couldn't get pin").into_output_low(),
-//            d2: gpio.get(13).expect("couldn't get pin").into_output_low(),
-//            d3: gpio.get(6).expect("couldn't get pin").into_output_low(),
-//            d4: gpio.get(5).expect("couldn't get pin").into_output_low(),
-//            d5: gpio.get(0).expect("couldn't get pin").into_output_low(),
-//            d6: gpio.get(11).expect("couldn't get pin").into_output_low(),
-//            d7: gpio.get(9).expect("couldn't get pin").into_output_low(),
-//            rs: gpio.get(21).expect("couldn't get pin").into_output_low(),
-//            en: gpio.get(20).expect("couldn't get pin").into_output_low(),
-//        }
