0cd174edd9e945d3d6cc758a45fc0829845ef6f7
eric artursson <eric.o.artursson@hotmail.com>
2023-04-06 18:09:58 +0200

    re added the usage of two different enums for power and cursorMode

--- src/configuration/mod.rs
+++ src/configuration/mod.rs
@@ -1,35 +1,41 @@
 use crate::*;
 
-// We can't always use bools, because sometimes I might need an "in-the-middle" option. So I made this one enum to controll multiple choice stuff
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Options {
+pub enum PowerMode {
     On,
     Off,
-    Blink,
 }
 
-// setts different settings for the lcd
-pub fn settings(pins: &mut Pins, cursor_mode: Options, power: Options) {
+// We can't always use bools, because sometimes I might need an "in-the-middle" option. So I made this one enum to controll multiple choice stuff
+
+pub enum CursorMode {
+    On,
+    Off,
+    Blink
+}
+
+// setts Cursor mode and PowerMode for the lcd
+pub fn settings(pins: &mut Pins, cursor_mode: CursorMode, power: PowerMode) {
     pins.rs.set_low();
 
-    if power == Options::On {
+    if power == PowerMode::On {
         match cursor_mode {
-            Options::On => bwrite(pins, 0b00001110),
-            Options::Blink => bwrite(pins, 0b00001111),
-            Options::Off => bwrite(pins, 0b00001100),
+            CursorMode::On => bwrite(pins, 0b00001110),
+            CursorMode::Blink => bwrite(pins, 0b00001111),
+            CursorMode::Off => bwrite(pins, 0b00001100),
         }
-    } else if power == Options::Off {
+    } else if power == PowerMode::Off {
         match cursor_mode {
-            Options::On => bwrite(pins, 0b00001010),
-            Options::Blink => bwrite(pins, 0b00001011),
-            Options::Off => bwrite(pins, 0b00001000),
+            CursorMode::On => bwrite(pins, 0b00001010),
+            CursorMode::Blink => bwrite(pins, 0b00001011),
+            CursorMode::Off => bwrite(pins, 0b00001000),
         }
     } else {
         println!("'{power:?}' is an invalid option")
     }
 }
 
-// sets up the lcd with some basic things
+// sets up the lcd with basic configuration
 pub fn begin(pins: &mut Pins, display_lines: i8 /*, bits: i8 */) {
     pins.rs.set_low();
 
@@ -46,6 +52,6 @@ pub fn begin(pins: &mut Pins, display_lines: i8 /*, bits: i8 */) {
     }
     clear(pins);
     home(pins);
-    settings(pins, Options::Off, Options::On);
+    settings(pins, CursorMode::Off, PowerMode::On);
     println!("finished setting up lcd. Use 'lcd::Settings();' to change screen settings");
 }
