f71911868981875b5726e586c38bc2aba584abec
eric artursson <eric.o.artursson@hotmail.com>
2023-04-08 13:38:18 +0200

    added a bad way of returning the error that occurs if a pin can't be used

--- src/pins/mod.rs
+++ src/pins/mod.rs
@@ -31,19 +31,33 @@ pub struct Pins {
 impl Pins {
     /// returns a struct with all the pins use to control the lcd
     /// you can toggle the pins on or off manually, but do not do that unless you really really know what you're doing
-    pub fn new(udf: DefinePins) -> Self {
+    pub fn new(udf: DefinePins) -> Result<Pins, rppal::gpio::Error> {
         let gpio = rppal::gpio::Gpio::new().expect("death");
-        Self {
-            d0: gpio.get(udf.d0).expect("couldn't get pin").into_output_low(),
-            d1: gpio.get(udf.d1).expect("couldn't get pin").into_output_low(),
-            d2: gpio.get(udf.d2).expect("couldn't get pin").into_output_low(),
-            d3: gpio.get(udf.d3).expect("couldn't get pin").into_output_low(),
-            d4: gpio.get(udf.d4).expect("couldn't get pin").into_output_low(),
-            d5: gpio.get(udf.d5).expect("couldn't get pin").into_output_low(),
-            d6: gpio.get(udf.d6).expect("couldn't get pin").into_output_low(),
-            d7: gpio.get(udf.d7).expect("couldn't get pin").into_output_low(),
-            rs: gpio.get(udf.rs).expect("couldn't get pin").into_output_low(),
-            en: gpio.get(udf.en).expect("couldn't get pin").into_output_low(),
-        }
+        let d0 = gpio.get(udf.d0)?.into_output();
+        let d1 = gpio.get(udf.d1)?.into_output();
+        let d2 = gpio.get(udf.d2)?.into_output();
+        let d3 = gpio.get(udf.d3)?.into_output();
+        let d4 = gpio.get(udf.d4)?.into_output();
+        let d5 = gpio.get(udf.d5)?.into_output();
+        let d6 = gpio.get(udf.d6)?.into_output();
+        let d7 = gpio.get(udf.d7)?.into_output();
+        let rs = gpio.get(udf.rs)?.into_output();
+        let en = gpio.get(udf.en)?.into_output();
+
+        Ok(Self{
+            d0,
+            d1,
+            d2,
+            d3,
+            d4,
+            d5,
+            d6,
+            d7,
+            rs,
+            en,
+        })
+
+
+
     }
 }
