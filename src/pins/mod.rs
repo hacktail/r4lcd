5f6b820224f681a2e6c21d757b9fabdd9c97c322
eric artursson <eric.o.artursson@hotmail.com>
2023-04-08 13:50:35 +0200

    Trying to add the ? into the struct we're sending back incase that might work

--- src/pins/mod.rs
+++ src/pins/mod.rs
@@ -1,8 +1,10 @@
 use crate::*;
-pub(crate) static PIN_FLAGS: [u8; 8] = [0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000];
+pub(crate) static PIN_FLAGS: [u8; 8] = [
+    0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000,
+];
 
 /// A struct which defines what pins you want to
-pub struct DefinePins{
+pub struct DefinePins {
     pub d0: u8,
     pub d1: u8,
     pub d2: u8,
@@ -32,32 +34,29 @@ impl Pins {
     /// returns a struct with all the pins use to control the lcd
     /// you can toggle the pins on or off manually, but do not do that unless you really really know what you're doing
     pub fn new(udf: DefinePins) -> Result<Pins, rppal::gpio::Error> {
-        let gpio = rppal::gpio::Gpio::new().expect("death");
-        let d0 = gpio.get(udf.d0)?.into_output();
-        let d1 = gpio.get(udf.d1)?.into_output();
-        let d2 = gpio.get(udf.d2)?.into_output();
-        let d3 = gpio.get(udf.d3)?.into_output();
-        let d4 = gpio.get(udf.d4)?.into_output();
-        let d5 = gpio.get(udf.d5)?.into_output();
-        let d6 = gpio.get(udf.d6)?.into_output();
-        let d7 = gpio.get(udf.d7)?.into_output();
-        let rs = gpio.get(udf.rs)?.into_output();
-        let en = gpio.get(udf.en)?.into_output();
+        let gpio = rppal::gpio::Gpio::new().expect("Couldn't get gpio att all");
+        //        let d0 = gpio.get(udf.d0)?.into_output();
+        //        let d1 = gpio.get(udf.d1)?.into_output();
+        //        let d2 = gpio.get(udf.d2)?.into_output();
+        //        let d3 = gpio.get(udf.d3)?.into_output();
+        //        let d4 = gpio.get(udf.d4)?.into_output();
+        //        let d5 = gpio.get(udf.d5)?.into_output();
+        //        let d6 = gpio.get(udf.d6)?.into_output();
+        //        let d7 = gpio.get(udf.d7)?.into_output();
+        //        let rs = gpio.get(udf.rs)?.into_output();
+        //        let en = gpio.get(udf.en)?.into_output();
 
-        Ok(Self{
-            d0,
-            d1,
-            d2,
-            d3,
-            d4,
-            d5,
-            d6,
-            d7,
-            rs,
-            en,
+        Ok(Self {
+            d0: gpio.get(udf.d0)?.into_output(),
+            d1: gpio.get(udf.d1)?.into_output(),
+            d2: gpio.get(udf.d2)?.into_output(),
+            d3: gpio.get(udf.d3)?.into_output(),
+            d4: gpio.get(udf.d4)?.into_output(),
+            d5: gpio.get(udf.d5)?.into_output(),
+            d6: gpio.get(udf.d6)?.into_output(),
+            d7: gpio.get(udf.d7)?.into_output(),
+            rs: gpio.get(udf.rs)?.into_output(),
+            en: gpio.get(udf.en)?.into_output(),
         })
-
-
-
     }
 }
