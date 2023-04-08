64b87093f76febf7776ae1a386e695c8ad2c2532
eric artursson <eric.o.artursson@hotmail.com>
2023-04-08 14:06:05 +0200

    removed some comments and now there's a real, concise way of returning the 'couldn't get pins error'

--- src/pins/mod.rs
+++ src/pins/mod.rs
@@ -35,16 +35,6 @@ impl Pins {
     /// you can toggle the pins on or off manually, but do not do that unless you really really know what you're doing
     pub fn new(udf: DefinePins) -> Result<Pins, rppal::gpio::Error> {
         let gpio = rppal::gpio::Gpio::new().expect("Couldn't get gpio att all");
-        //        let d0 = gpio.get(udf.d0)?.into_output();
-        //        let d1 = gpio.get(udf.d1)?.into_output();
-        //        let d2 = gpio.get(udf.d2)?.into_output();
-        //        let d3 = gpio.get(udf.d3)?.into_output();
-        //        let d4 = gpio.get(udf.d4)?.into_output();
-        //        let d5 = gpio.get(udf.d5)?.into_output();
-        //        let d6 = gpio.get(udf.d6)?.into_output();
-        //        let d7 = gpio.get(udf.d7)?.into_output();
-        //        let rs = gpio.get(udf.rs)?.into_output();
-        //        let en = gpio.get(udf.en)?.into_output();
 
         Ok(Self {
             d0: gpio.get(udf.d0)?.into_output(),
